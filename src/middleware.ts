4a3e6c42388d6981e98890f3a753ae7e7d324726
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2025-05-08 09:16:32 +0800

    Redirect to host

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,8 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
   const user = req.auth?.user;
 
-  const url = "http://localhost:3000";
+  // const url = "http://localhost:3000";
+  const url = "https://b-dazzle-cafe.vercel.app"; 
   const isRootRoute = req.nextUrl.pathname === "/";
   const isAuthRoute = req.nextUrl.pathname.includes("/account/");
   const isPrivateRoute = privateRoutes.includes(req.nextUrl.pathname);
